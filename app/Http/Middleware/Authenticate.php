f95fbae630b648ba129d6243ad04fd434579565f
alfin ardian <developer.alfin@gmail.com>
2021-07-11 16:01:03 +0700

    Handling authentication

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,7 +14,10 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
+        if ($request->expectsJson()) {
+            return response()->json(['state' => 0, 'message' => 'Unauthenticated'], 401);
+            
+        }else{
             return route('login');
         }
     }
