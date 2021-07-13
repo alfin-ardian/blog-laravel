35a90499b4d35ba6f70fd68efc278a718a50e9c0
alfin ardian <developer.alfin@gmail.com>
2021-07-14 05:47:33 +0700

    react website

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,15 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Website route
+Route::get('/{path?}', function () {
+    return view('website');
+})->where('path', '[^admin]*');
+// Admin route
+Route::get('/admin/{path?}', function () {
+    return view('admin');
+})->where('path', '.*');
